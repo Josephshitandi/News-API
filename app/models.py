80358649da70c8f46762d3a42050e17c5d54e304
Josephshitandi <jsphshtnd@gmail.com>
2020-10-17 10:50:36 +0300

    Add class news that defines News objects

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class News:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,title,image,description,date):
+        self.title = title
+        self.image = image
+        self.description = description
+        self.date = date
