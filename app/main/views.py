13618c791805147fe09cf2a953c83c7ec3bb0235
Josephshitandi <jsphshtnd@gmail.com>
2020-10-17 11:59:33 +0300

    Add index() in views.py that that returns the index page and its data

---
+++ app/main/views.py
@@ -0,0 +1,30 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_news
+from .forms import ReviewForm
+# from ..models import Review
+
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting news sources
+    popularity = get_news('popularity')
+    bitcoin = get_news('bitcoin')
+    business = get_news('business')
+    techcrunch = get_news('techcrunch')
+    wall_street = get_news('wsj')
+
+    title = 'Home - Welcome to The best News Review Website Online'
+
+    search_news = request.args.get('news_query')
+
+    if search_news:
+        return redirect(url_for('search',news_name=search_news))
+    else:
+        return render_template('index.html', title = title, popularity = popularity, bitcoin = bitcoin, business = business, techcrunch = techcrunch, wall_street = wall_street )
+
+
