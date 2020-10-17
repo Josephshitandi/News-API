105ef319de48d2a2ec1b40bc0389551ea39fa59c
Josephshitandi <jsphshtnd@gmail.com>
2020-10-17 15:17:36 +0300

    Add Article with its methods

--- app/models.py
+++ app/models.py
@@ -9,3 +9,31 @@ class News:
         self.description = description
         self.date = date
         
+class Article:
+
+    all_articles = []
+
+    def __init__(self,title,image,article):
+        self.title = title
+        self.imageurl = imageurl
+        self.article = article
+
+
+    def save_article(self):
+        article.all_articles.append(self)
+
+
+    @classmethod
+    def clear_articles(cls):
+        article.all_articles.clear()
+
+    @classmethod
+    def get_articles(cls,title):
+
+        response = []
+
+        for article in cls.all_articles:
+            if article.title == title:
+                response.append(article)
+
+        return response
